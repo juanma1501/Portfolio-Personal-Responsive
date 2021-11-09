22f3243b70365074d81b9ff1025d777c4ce869e7
Juan Manuel Porrero Almansa <juanmaporreroalmansa@gmail.com>
2021-11-09 23:22:56 +0100

    Header y parte de la sección Home

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// ============================================ DARK THEME ============================================
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+//PREVIOUSLY SELECTED TOPIC (check desde el almacenamiento local)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtenemos el tema actual que tiene la interfaz
+const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme)?'uil-moon':'uil-sun'
+
+//Ahora comprobamos que el usuario ha elegido previamente un topic
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark'?'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon'?'add':'remove'](iconTheme)
+}
+
+// Activar / desactivar el tema manualmente con el botón
+themeButton.addEventListener('click', () => {
+    //añadir o borrar el tema claro/oscuro (iconos)
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    //Tenemos que guardar el tema en el icono actual que el usuario ha elegido
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+console.log("LA SELECCIÓN DE TEMA FUNCIONA")
+
+
+// ============================================ MENU SHOW / HIDDEN ============================================
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+// ========== MENU SHOW ==========
+/* Validar si la constante existe */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// ========== MENU HIDE ==========
+/* Validar si la constante existe */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+console.log("MENU SETTINGS FUNCIONA")
+
+// ============================================ REMOVE MENU  ============================================
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu= document.getElementById('nav-menu')
+    //cuando hacemos click en nav__links, quitamos el menu
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+console.log("Quitar menu FUNCIONA")
+
+
+// ============================================ Typewriter Effect  ============================================
+new Typewriter('#typewriter', {
+  strings: ['Juan Manuel Porrero Almansa', 'Estudiante de Ingeniería Informática', 'Cuarto curso'],
+  autoStart: true,
+  loop: true,
+  cursor: "|"
+});
+
+console.log("Typewriter FUNCIONA")
