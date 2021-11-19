b8b73db13ce124384e69029acde40fc3d199be79
Juan Manuel Porrero Almansa <juanmaporreroalmansa@gmail.com>
2021-11-19 20:06:29 +0100

    Scroll e iluminación de secciones del header

--- js/script.js
+++ js/script.js
@@ -109,3 +109,28 @@ function scrollUp(){
 }
 
 window.addEventListener('scroll', scrollUp)
+
+
+//===================================== SCROLL SECTION ACTIVE HIGHLIGHT =================================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+
+    console.log("Marcador de sección FUNCIONA")
+}
+window.addEventListener('scroll', scrollActive)
